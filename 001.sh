e6c9d429467fee400d5086168251753f02b2dbad
Alberto C <acs.corti@gmail.com>
2021-04-28 20:55:06 +0200

    new file acs

---
+++ 001.sh
@@ -0,0 +1,4 @@
+ls -SL ~/data/opentraveldata | tail -n 1
+ls -SL ~/data/opentraveldata |head -n 2 |  tail -n 1
+ls -Sl  ~/data/opentraveldata |head -n 2 |  tail -n 1
+ls -Sl ~/data/opentraveldata | head -n 2 | tail -n 1
